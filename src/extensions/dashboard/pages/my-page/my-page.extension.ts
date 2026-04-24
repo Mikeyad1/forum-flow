0c9ba5d7ad888c87db7e2bb916ba30191a496e86
Mikey Adam <mikey.adam33@gmail.com>
2026-04-24 18:17:28 +0300

    initial commit

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '0f1dd78c-1399-4e6b-b76e-373ea40a7e92',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
